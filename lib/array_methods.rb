587e340266aa5bf11b711e1cf67e55c41cce8d66
Yasmin Torres <yasmintorres90@hotmail.com>
2018-09-30 20:26:28 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+  famous_cats = ["Grafield", "Heathclif", "Moris the Cat"]
+  famous_cats.include?
 end
 
 def using_sort(array)
-
+  cars = ["Toyota", "Ford", "Bentley"]
+  cars.sort
 end
 
 def using_reverse(array)
-
+  colors = ["Red", "Yellow", "Black"]
+  colors.reverse
 end
 
 def using_first(array)
-
+  football_teams = ["Giants", "Eagles", "Lions"]
+  football_teams.first
 end
 
 def using_last(array)
-
+  holidays = ["Halloween", "Christmas", "Valentine"]
+  holidays.last
 end
 
 def using_size(array)
-
+  flowers ["Roses", "Daisies", "Tulips"]
+  flowers.size
 end
